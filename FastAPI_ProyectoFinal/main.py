1131c0cdc04689fae63c8c4b8e80bb6f84e8f946
unknown <ivg77850@lasallebajio.edu.mx>
2024-03-11 19:11:25 -0600

    A jalar dieguiño

--- FastAPI_ProyectoFinal/main.py
+++ FastAPI_ProyectoFinal/main.py
@@ -6,39 +6,41 @@ from typing import Optional, List
 app = FastAPI()
 app.title = "PROYECTO 1er Parcial API"
 
-
 books = [
     {
         "id" : 1,
-        "title" : "harry potter and the chamber of secrets",
-        "author" : "k.K Rowling",
+        "title" : "Harry Potter y la Camara de los secretos",
+        "author" : "J.K Rowling",
         "year" : 1998,
-        "category" : "Fantasia",
-        "pages" : 251,
+        "category" : {
+            "id": 1,
+            "name": "Fantasia",
+        },
+        "pages" : 320,
+    },
+    {
+        "id" : 2,
+        "title" : "harry potter philosopher's stone",
+        "author" : "J.K Rowling",
+        "year" : 1997,
+        "category" : {
+            "id": 1,
+            "name": "Fantasia",
+        },
+        "pages" : 320,
     }
 ]
-    # {
-    #     "id" : 2,
-    #     "title" : "harry potter philosopher's stone",
-    #     "author" : "J.K Rowling",
-    #     "year" : 1997,
-    #     "category" : {
-    #         "id": 1,
-    #         "name": "Fantasia",
-    #     },
-    #     "pages" : 320,
-    # }
 
-categories = []
-    # {
-    #     "id": 1,
-    #     "name": "Fantasia",
-    # },
-    # {
-    #     "id": 2,
-    #     "name": "Misterio",
-    # }
-
+categories = [
+    {
+        "id": 1,
+        "name": "Fantasia",
+    },
+    {
+        "id": 2,
+        "name": "Misterio",
+    }
+]
 
 class Category(BaseModel):
     id : int = Field(ge = 1)
@@ -57,7 +59,7 @@ class Book(BaseModel):
     title: str = Field(min_length=1,max_length=30)
     author: str = Field(min_length=1,max_length=50)
     year: str
-    category: str
+    category: Category
     pages: int = Field(ge=1)
     class config:
 
@@ -66,13 +68,15 @@ class Book(BaseModel):
                 "title": "Titulo del libro",
                 "author": "Autor del libro",
                 "year": "Año de publicacion",
-                "category": "Misterio",
+                "category": {
+                    "id" : 1,
+                    "name" : "Fantasy"
+                },
                 "pages": 1
             }
         }
 
 # Contador global para id
-        
 class AutoIncrement:
     _id = 0
 
@@ -80,13 +84,13 @@ class AutoIncrement:
     def get_id(cls):
         cls._id += 1
         return cls._id
-    
+
     def delete_int(cls):
         cls._id -= 1
         return cls._id
 
 auto_increment_id_categories = AutoIncrement()
-auto_increment_id_books = AutoIncrement()  
+auto_increment_id_books = AutoIncrement()
 # auto_increment_id_categories = len(categories) + 1
 # next_book_id = len(books) + 1
 
@@ -124,18 +128,18 @@ def get_books_by_category(category:str):
 def create_book(title: str = Body(), author: str = Body(), year: str = Body(), category: str = Body(), pages: int = Body()):
     if not any(cat["name"].lower().strip() == category.lower().strip() for cat in categories):
         return JSONResponse(status_code=400, content={"message": f"La categoría '{category}' no existe."})
-    
+
     global auto_increment_id_books
     # Crea una nueva instancia para el modelo book
     new_book = {
-                "id" : auto_increment_id_books.get_id(),
-                "title": title,
-                "author": author,
-                "year": year,
-                "category": category,
-                "pages": pages
+                    "id" : auto_increment_id_books.get_id(),
+                    "title": title,
+                    "author": author,
+                    "year": year,
+                    "category": category,
+                    "pages": pages
                 }
-    
+
     # Agregar un nuevo libro
     books.append(new_book)
     return books
@@ -161,7 +165,7 @@ def modify_book(id: int = Path(), title: str = Body(), author: str = Body(), yea
     # Check if the provided id exists in the list of books
     if not any(book['id'] == id for book in books):
         return JSONResponse(status_code=400, content={"message": f"El libro con el id {id} no existe."})
-
+    
     # Update the book with the provided id
     for book in books:
         if book['id'] == id:
@@ -214,23 +218,21 @@ def delete_category(name : str = Query):
 
             for index, category in enumerate(categories, start=1):
                 category['id'] = index
-            return JSONResponse(status_code = 200, content = {"message" : "La categoria se elimino correctamente"})
     return JSONResponse(status_code = 400, content = {"message" : "No existe tal categoria"})
 
 # Modificar categorias
 @app.put('/categories/{name}', tags=["Categories"], status_code=200)
 def modify_category(name: str = Query, newName: str = Query):
     global books
-    updated_books = []
     for item in categories:
         if item["name"].lower().strip().replace(" ", "") == name.lower().strip().replace(" ", ""):
             item["name"] = newName
             for book in books:
-                if book["category"].lower().strip().replace(" ", "") == name.lower().strip().replace(" ", ""):
-                    book["category"] = newName
-                updated_books.append(book)
-            return JSONResponse(status_code=200, content={"message": "La categoría se modificó correctamente.", "updated_books": updated_books})
+                if book["category"]["name"].lower().strip().replace(" ", "") == name.lower().strip().replace(" ", ""):
+                    book["category"]["name"] = newName
+            return JSONResponse(status_code=200, content={"message": "La categoría se modificó correctamente."})
     return JSONResponse(status_code=400, content={"message": "No existe tal categoría."})
+
 # @app.put('/categories/{name}', tags = ["Categories"], status_code = 200)
 # def modify_category(name : str = Query, newName : str = Query):
 #     for item in categories:
